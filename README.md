b002d20905851377ac7cd16d429b489d89329321
Johnny <johnrussoofficial@outlook.com>
2020-11-10 17:41:53 -0500

    Beautiful Soup web scrapping functionality added

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## This-Date-in-History-Python
-Python app, generates random dates. Early Development
+Python app, generates random dates and returns bits of trivia surrounding that date
+
+Updates: 
+  - App now uses Beautiful Soup to scrape Wikipedia for several trivia tidbits for each date generated
 
 Plans:
-  - Find info on random dates generated (external API?)
   - backend interactions, save facts found
 
--- this_date.py
+++ this_date.py
@@ -1,12 +1,13 @@
 """Find a random date in history for trivia facts."""
 import sys
 import random
+import requests
+from bs4 import BeautifulSoup
 
 
 def main():
     """Code returns random dates. More features to be added, like a This Date in History Look-up."""
     print("This is the Random Date Generator!\n")
-    print("Your lucky date is...\n\n")
 
     months = ('January', 'February', 'March', 'April', 'May',
               'June', 'July', 'August', 'September', 'October',
@@ -24,16 +25,32 @@ def main():
             date_month = random.choice(no_feb)
         else:
             date_month = random.choice(months)
+        
+        response = requests.get(url="https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/" +
+                                date_month +
+                                "_" +
+                                str(date_day))
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.find(class_="mw-parser-output").contents[6]
+        add_content = soup.find(class_="mw-parser-output").contents[10].find_all("li")
 
+        title_text = ""
+        for i in title:
+            title_text += i.string
+
+        print("Your lucky date is...{} {}".format(date_month.upper(), str(date_day).upper()), file=sys.stderr)
         print("\n\n")
-        print("{} {}".format(date_month, date_day), file=sys.stderr)
-        print("\n\n")
+        print(title_text)
+        print("----Additional events of importance----")
+        for j in add_content:
+            add_text = ""
+            for all in j.contents:
+                add_text += all.string
+            print(add_text)
 
         try_again = input("\nTry Again? (Press Enter, or Press 'n' to quit)\n")
         if try_again.lower() == "n":
             break
-
-    input("\nPress Enter to exit")
 
 if __name__ == "__main__":
     main()
